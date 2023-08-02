d004676ac5ef3a1ac37c7835d58ed8bca3e9b46e
anouar <anouar.bakouch@outlook.fr>
2023-08-02 19:18:00 +0100

    establishing the architecture

---
+++ BudgetPredFrontEnd/src/models/Budget.ts
@@ -0,0 +1,11 @@
+
+
+export interface Budget {
+    IDEIMPST : number;
+    MONTSTRU : number;
+    MONTRAPP : number;
+    MOISSOLD : Date;
+    CODYTPAC : string;
+    LIBACTGE : string;
+    Budgets : number;
+}
