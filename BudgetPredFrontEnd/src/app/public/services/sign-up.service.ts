e6afd973d879cd2579dd023f553b2b0dd90ef6d6
anouar <anouar.bakouch@outlook.fr>
2023-08-03 16:58:12 +0100

    implemented sign up and sign in

--- BudgetPredFrontEnd/src/app/public/services/sign-up.service.ts
+++ BudgetPredFrontEnd/src/app/public/services/sign-up.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SignUpService {
+export class SignupService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  signup(username: string, email: string, password: string, firstName: string, lastName: string) {
+    const url = environment.apiUrl+'/signup';
+    const body = {
+      username: username,
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName
+    };
+    return this.http.post(url, body);
+  }
+
 }
--- BudgetPredFrontEnd/src/app/public/sign-up/sign-up.component.ts
+++ BudgetPredFrontEnd/src/app/public/sign-up/sign-up.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SignupService } from '../services/sign-up.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent {
+
+export class SignUpComponent implements OnInit {
+
+  username: string="";
+  email: string="";
+  password: string="";
+  firstName: string="";
+  lastName: string="";
+
+  constructor(private signupService: SignupService) {}
+
+  ngOnInit() {}
+
+  signup() {
+    this.signupService.signup(this.username, this.email, this.password, this.firstName, this.lastName)
+      .subscribe(
+        (success) => {
+          if (success) {
+            // The user has successfully signed up.
+          } else {
+            // The signup failed.
+          }
+        },
+        (error) => {
+          // An error occurred while signing up.
+        }
+      );
+  }
 
 }
