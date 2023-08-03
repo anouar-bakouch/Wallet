6c5261de548ae84b595ce0048a26b02a1c416c7f
anouar <anouar.bakouch@outlook.fr>
2023-08-03 05:21:02 +0100

    implementation of login

--- BudgetPredFrontEnd/src/app/public/login/login.component.ts
+++ BudgetPredFrontEnd/src/app/public/login/login.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../services/login.service';
+import { User } from 'src/models/User';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
-}
+  export class LoginComponent {
+  
+    constructor(private loginService: LoginService) { }
+  
+    login(username: string, password: string): void {
+      this.loginService.login(username, password).subscribe(
+        (user: User) => {
+          // Handle successful login
+          console.log('Login successful', user);
+          // Further actions (e.g., redirect to a different page)
+        },
+        (error) => {
+          // Handle login error
+          console.error('Login error', error);
+        }
+      );
+    }
+  }
+
--- BudgetPredFrontEnd/src/app/public/services/login.service.ts
+++ BudgetPredFrontEnd/src/app/public/services/login.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class LoginService {
 
-  constructor() { }
+  private url:string = environment.apiUrl;
+
+  constructor(private http:HttpClient) { }
+
+  public login(username:string, password:string){ 
+    return this.http.post<any>(this.url+"/login", {username, password});
+  }
+
 }
