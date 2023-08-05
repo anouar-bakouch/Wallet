17999e3d4c9560bd6cab4d768d1db3952e911542
anouar <anouar.bakouch@outlook.fr>
2023-08-05 18:47:17 +0100

    added guards

--- BudgetPredFrontEnd/src/app/app-routing.module.ts
+++ BudgetPredFrontEnd/src/app/app-routing.module.ts
@@ -1,27 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IsUserGuard } from './guards/is-user.guard';
 
 const routes: Routes = [
   {
-    path : '',
-    redirectTo : 'home/infos',
-    pathMatch : 'full'
+    path: '',
+    redirectTo: 'home/infos',
+    pathMatch: 'full'
   },
   {
-    path : 'home',
-    loadChildren : () => import('./public/public.module').then(m => m.PublicModule)
+    path: 'home',
+    loadChildren: () => import('./public/public.module').then(m => m.PublicModule)
   },
   {
-    path : 'budgetHome',
-    loadChildren : () => import('./core/core.module').then(m => m.CoreModule)
+    path: 'budgetHome',
+    loadChildren: () => import('./core/core.module').then(m => m.CoreModule),
+    canActivate: [IsUserGuard] // Apply the AuthGuard to this route
   }
-  
-
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-
 export class AppRoutingModule { }
