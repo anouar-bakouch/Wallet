1f04cfa28c9fcaf03cc87e269c2d4cbd94f28e39
anouar <anouar.bakouch@outlook.fr>
2023-08-05 18:59:06 +0100

    constants

--- BudgetPredFrontEnd/src/app/auth/auth.service.ts
+++ BudgetPredFrontEnd/src/app/auth/auth.service.ts
@@ -1,36 +1,40 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
+  private currentUserSubject = new BehaviorSubject<any>(null);
+  private url:string = environment.apiUrl;
 
   constructor() { }
 
-  login(): Observable<boolean> {
-    // Simulating a login request
+  login(username: string, password: string): Observable<boolean> {
     return new Observable<boolean>((observer) => {
-      // Perform the login logic here
-      // Example:
-      // if (username === 'admin' && password === 'password') {
-      //   this.isAuthenticatedSubject.next(true);
-      //   observer.next(true);
-      // } else {
-      //   observer.next(false);
-      // }
-      // observer.complete();
+        // TODO: call the API to authenticate the user
+        // if the authentication is successful, call next() on the isAuthenticatedSubject
+        // if the authentication fails, call error() with an appropriate error message
+
+        
+
+        
     });
   }
 
-  logout(): void {
-    // Perform the logout logic here
-    // Example:
-    // this.isAuthenticatedSubject.next(false);
+  logout(): void {  
+  
+    this.isAuthenticatedSubject.next(false);
+    this.currentUserSubject.next(null);
   }
 
   isAuthenticated(): Observable<boolean> {
     return this.isAuthenticatedSubject.asObservable();
+  }
+
+  getCurrentUser(): Observable<any> {
+    return this.currentUserSubject.asObservable();
   }
 }
---
+++ BudgetPredFrontEnd/src/app/utils/constants.ts
@@ -0,0 +1,18 @@
+const TOKEN_KEY = 'tk';
+const REFRESH_TOKEN_KEY = 'rtk';
+const STATUS_AUTH_OK = 'status_auth_ok';
+const USER_INFO = 'uinfo';
+const TOKEN_INSERT_DATE_TIME = 'tkdt';
+
+const USER_GROUPE_NAME = 'user';
+const ADMIN_GROUPE_NAME = 'admin';
+
+export {
+    TOKEN_KEY,
+    REFRESH_TOKEN_KEY,
+    STATUS_AUTH_OK,
+    USER_INFO,
+    TOKEN_INSERT_DATE_TIME,
+    USER_GROUPE_NAME,
+    ADMIN_GROUPE_NAME
+}
