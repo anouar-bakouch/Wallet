35e4026732cc61e2d221819f247fc1525aed5755
anouar <anouar.bakouch@usmba.ac.ma>
2023-08-09 19:36:59 +0100

    buidling a classification model

---
+++ BudgetPredBackEnd/models/Classification.model.py
@@ -0,0 +1,11 @@
+#  sequential recommendation techniques to predict future items that a user might be interested in. 
+
+# In[1]: Importing the libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
+
